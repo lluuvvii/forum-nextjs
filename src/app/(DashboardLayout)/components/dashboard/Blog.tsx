64e563373d63aea7fc6b1613b1b060a19ab7f160
lluuvvii <kurniawanluvi64@gmail.com>
2024-05-02 13:02:38 +0700

    build and fix some errors

--- src/app/(DashboardLayout)/components/dashboard/Blog.tsx
+++ src/app/(DashboardLayout)/components/dashboard/Blog.tsx
@@ -8,10 +8,10 @@ import {
   Tooltip,
   Fab,
 } from "@mui/material";
-import img1 from "public/images/products/s4.jpg";
-import img2 from "public/images/products/s5.jpg";
-import img3 from "public/images/products/s7.jpg";
-import img4 from "public/images/products/s11.jpg";
+import img1 from "@/../public/images/products/s4.jpg";
+import img2 from "@/../public/images/products/s5.jpg";
+import img3 from "@/../public/images/products/s7.jpg";
+import img4 from "@/../public/images/products/s11.jpg";
 import { Stack } from "@mui/system";
 import { IconBasket } from "@tabler/icons-react";
 import BlankCard from "@/app/(DashboardLayout)/components/shared/BlankCard";
--- src/app/(DashboardLayout)/layout/sidebar/Updrade.tsx
+++ src/app/(DashboardLayout)/layout/sidebar/Updrade.tsx
@@ -1,5 +1,5 @@
 import { Box, Typography, Button } from '@mui/material';
-import img1 from 'public/images/backgrounds/rocket.png';
+import img1 from '@/../public/images/backgrounds/rocket.png';
 import Image from 'next/image';
 import Link from 'next/link';
 
