38e912846091ef680276d5b1ae29a0ec13c60dea
lluuvvii <kurniawanluvi64@gmail.com>
2024-05-03 15:04:02 +0700

    reset wysiwyg

--- src/app/(DashboardLayout)/sample-page/page.tsx
+++ src/app/(DashboardLayout)/sample-page/page.tsx
@@ -2,15 +2,12 @@
 import { Typography, Grid, Button } from '@mui/material';
 import PageContainer from '@/app/(DashboardLayout)/components/container/PageContainer';
 import DashboardCard from '@/app/(DashboardLayout)/components/shared/DashboardCard';
-import ReactQuill from 'react-quill'
-import 'react-quill/dist/quill.snow.css'
-import { useState, useCallback, useRef, useEffect } from 'react'
+import { useState } from 'react'
 import Axios from 'axios'
 
 
 const SamplePage = () => {
   const [value, setValue] = useState('')
-  const reactQuillRef = useRef<ReactQuill>(null)
   const [answer, setAnswer] = useState(false)
 
   const handleAnswer = () => {
@@ -32,44 +29,9 @@ const SamplePage = () => {
     return data
   }
 
-  const imageHandler = useCallback(() => {
-  }, [])
-
-  const modules = {
-    toolbar: {
-      container: [
-        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-        ['blockquote', 'code-block'],
-        ['link', 'image', 'video', 'formula'],
-
-        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-        [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'list': 'check' }],
-        [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
-        [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
-        [{ 'direction': 'rtl' }],                         // text direction
-
-        [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-
-        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-        [{ 'font': [] }],
-        [{ 'align': [] }],
-
-        ['clean']                                         // remove formatting button
-      ],
-      handlers: {
-        image: imageHandler
-      },
-      clipboard: {
-        matchVisual: false
-      }
-    }
-  }
-
-  // console.log({ value })
-
   return (
     <PageContainer title="Sample Page" description="this is Sample page">
+      <div>hallo</div>
       <DashboardCard title="Sample Page">
         <Typography>This is a sample page</Typography>
       </DashboardCard>
@@ -82,14 +44,7 @@ const SamplePage = () => {
         {answer &&
           <Grid container spacing={2} sx={{ mb: 3 }}>
             <Grid item xs={12} style={{ height: 390 }}>
-              <ReactQuill
-                ref={reactQuillRef}
-                theme="snow"
-                value={value}
-                onChange={setValue}
-                modules={modules}
-                style={{ height: "300px" }}
-              />
+              {/* wysiwys here */}
             </Grid>
             <Grid item>
               <Button variant="contained" onClick={handleAnswer}>Upload Jawaban</Button>
