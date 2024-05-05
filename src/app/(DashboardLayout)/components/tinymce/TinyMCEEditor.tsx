b8e634b435cf5561de2a217c66205389ef976501
lluuvvii <kurniawanluvi64@gmail.com>
2024-05-05 22:28:21 +0700

    fix error

--- src/app/(DashboardLayout)/components/tinymce/TinyMCEEditor.tsx
+++ src/app/(DashboardLayout)/components/tinymce/TinyMCEEditor.tsx
@@ -2,25 +2,8 @@ import React from 'react'
 import { Editor } from '@tinymce/tinymce-react'
 import Axios from 'axios'
 
-const TinyMCEEditor = ({ onEditorChange }: any) => {
-  const uploadToCLoudinary = async (blobInfo: any, success: any, failure: any) => {
-    const formData = new FormData()
-    formData.append("file", blobInfo.blob(), blobInfo.filename())
-    formData.append(
-      "upload_preset",
-      "forum_image_upload"
-    )
-    formData.append('folder', 'forum_nextjs')
-
-    const response = await Axios.post('https://api.cloudinary.com/v1_1/dbzjr3io4/image/upload', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      }
-    })
-    const data = await response.data.url
-    success(data)
-    return data
-  }
+const TinyMCEEditor = ({ onEditorChange, uploadToCLoudinary }: any) => {
+  
   return (
     <Editor
       apiKey='2rbge8xcacyk5ou86hjs033lq6o9nwi71b3weee3ae79cjwt'
--- src/app/(DashboardLayout)/sample-page/page.tsx
+++ src/app/(DashboardLayout)/sample-page/page.tsx
@@ -11,23 +11,23 @@ const SamplePage = () => {
   const editorRef = useRef(null)
   const [answer, setAnswer] = useState(false)
 
-  // const uploadToCLoudinary = async (blobInfo: any, success: any, failure: any) => {
-  //   const formData = new FormData()
-  //   formData.append("file", blobInfo.blob(), blobInfo.filename())
-  //   formData.append(
-  //     "upload_preset",
-  //     "forum_image_upload"
-  //   )
-  //   formData.append('folder', 'forum_nextjs')
-
-  //   const response = await Axios.post('https://api.cloudinary.com/v1_1/dbzjr3io4/image/upload', formData, {
-  //     headers: {
-  //       'Content-Type': 'multipart/form-data'
-  //     }
-  //   })
-  //   const data = await response.data.url
-  //   success(data)
-  // }
+  const uploadToCLoudinary = async (blobInfo: any, success: any, failure: any) => {
+    const formData = new FormData()
+    formData.append("file", blobInfo.blob(), blobInfo.filename())
+    formData.append(
+      "upload_preset",
+      "forum_image_upload"
+    )
+    formData.append('folder', 'forum_nextjs')
+    const response = await Axios.post('https://api.cloudinary.com/v1_1/dbzjr3io4/image/upload', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+    const data = await response.data.url
+    success(data)
+    return data
+  }
 
   const handleChange = (content: any, editor: any) => {
     setValue(content)
@@ -58,7 +58,7 @@ const SamplePage = () => {
                 <Grid container spacing={2}>
                   <Grid item xs={12} style={{ height: 390 }} sx={{ mb: 3 }}>
                     {/* wysiwys here */}
-                    <TinyMCEEditor onEditorChange={handleChange} />
+                    <TinyMCEEditor onEditorChange={handleChange} uploadToCLoudinary={uploadToCLoudinary} />
                   </Grid>
                   <Grid item>
                     <Button variant="contained" onClick={handleAnswer}>Unggah Pertanyaan</Button>
